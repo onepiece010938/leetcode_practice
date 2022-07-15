0e512bd1678dc2fafbf8abf50de5731418a28372
raymond <onepiece0093@gmail.com>
2022-07-15 10:16:15 +0800

    🎉 init: first commit

---
+++ 206 Reverse Linked List.go
@@ -0,0 +1,21 @@
+package main
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func reverseList(head *ListNode) *ListNode {
+	
+	return head
+}
+
+func main(){
+
+}
