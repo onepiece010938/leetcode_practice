41e2be79b7fb47909311c50f261f68b55ce32ff0
raymond <onepiece0093@gmail.com>
2022-07-20 10:17:47 +0800

    ✨ feat(142): add map & pointer solution

---
+++ 142-LinkedListCycleII/142-LinkedListCycleII.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+Given the head of a linked list, return the node where the cycle begins. If there is no cycle, return null.
+
+There is a cycle in a linked list if there is some node in the list that can be reached again by continuously
+
+following the next pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to (0-indexed).
+
+It is -1 if there is no cycle. Note that pos is not passed as a parameter.
+
+Do not modify the linked list.
+
+Input: head = [3,2,0,-4], pos = 1
+Output: tail connects to node index 1
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+/
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+*/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func detectCycle(head *ListNode) *ListNode {
+	pos_map := make(map[*ListNode]bool, 0)
+	i := 0
+	for head != nil {
+		if pos_map[head.Next] {
+			return head.Next
+		}
+		pos_map[head] = true
+		head = head.Next
+		i++
+	}
+	return nil
+}
+
+/*
+相遇时： slow指针走过的节点数为: x + y， fast指针走过的节点数： x + y + n (y + z)，n为fast指针在环内走了n圈才遇到slow指针， （y+z）为 一圈内节点的个数A。
+
+因为fast指针是一步走两个节点，slow指针一步走一个节点， 所以 fast指针走过的节点数 = slow指针走过的节点数 * 2：
+
+(x + y) * 2 = x + y + n (y + z)
+
+两边消掉一个（x+y）: x + y = n (y + z)
+
+因为要找环形的入口，那么要求的是x，因为x表示 头结点到 环形入口节点的的距离。
+
+所以要求x ，将x单独放在左面：x = n (y + z) - y ,
+
+再从n(y+z)中提出一个 （y+z）来，整理公式之后为如下公式：x = (n - 1) (y + z) + z 注意这里n一定是大于等于1的，因为 fast指针至少要多走一圈才能相遇slow指针。
+
+这个公式说明什么呢？
+
+先拿n为1的情况来举例，意味着fast指针在环形里转了一圈之后，就遇到了 slow指针了。
+
+当 n为1的时候，公式就化解为 x = z，
+
+这就意味着，从头结点出发一个指针，从相遇节点 也出发一个指针，这两个指针每次只走一个节点， 那么当这两个指针相遇的时候就是 环形入口的节点。
+
+也就是在相遇节点处，定义一个指针index1，在头结点处定一个指针index2。
+
+让index1和index2同时移动，每次移动一个节点， 那么他们相遇的地方就是 环形入口的节点。
+那么 n如果大于1是什么情况呢，就是fast指针在环形转n圈之后才遇到 slow指针。
+
+其实这种情况和n为1的时候 效果是一样的，一样可以通过这个方法找到 环形的入口节点，只不过，index1 指针在环里 多转了(n-1)圈，然后再遇到index2，相遇点依然是环形的入口节点。
+*/
+/*
+func detectCycle(head *ListNode) *ListNode {
+    slow, fast := head, head
+    for fast != nil && fast.Next != nil {
+        slow = slow.Next
+        fast = fast.Next.Next
+        if slow == fast {
+            for slow != head {
+                slow = slow.Next
+                head = head.Next
+            }
+            return head
+        }
+    }
+    return nil
+}
+*/
+func main() {
+	head := ListNode{Val: 1, Next: &ListNode{Val: 2, Next: &ListNode{Val: 3, Next: &ListNode{Val: 4, Next: &ListNode{Val: 5, Next: &ListNode{Val: 6, Next: &ListNode{Val: 7, Next: nil}}}}}}}
+	head.Next.Next.Next.Next.Next.Next = head.Next
+	newhead := detectCycle(&head)
+	fmt.Println("ANS:", newhead)
+	fmt.Println("ANS:", newhead)
+}
