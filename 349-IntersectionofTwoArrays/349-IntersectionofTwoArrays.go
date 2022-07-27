4fcaeacbd10855cd60b3419bdf82b05a0e046d10
raymond <onepiece0093@gmail.com>
2022-07-27 10:22:01 +0800

    feat(349):add my solution

---
+++ 349-IntersectionofTwoArrays/349-IntersectionofTwoArrays.go
@@ -0,0 +1,47 @@
+package main
+
+import "fmt"
+
+/*
+Given two integer arrays nums1 and nums2, return an array of their intersection.
+Each element in the result must be unique and you may return the result in any order.
+
+
+
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+Explanation: [4,9] is also accepted.
+
+
+Constraints:
+
+1 <= nums1.length, nums2.length <= 1000
+0 <= nums1[i], nums2[i] <= 1000
+*/
+
+func intersection(nums1 []int, nums2 []int) []int {
+	var ans []int
+	nums1_map := make(map[int]bool)
+	for _, val := range nums1 {
+		nums1_map[val] = true
+	}
+	for _, val := range nums2 {
+		if nums1_map[val] {
+			ans = append(ans, val)
+			nums1_map[val] = false
+		}
+	}
+	return ans
+}
+
+func main() {
+	num1 := []int{1, 2, 2, 1}
+	num2 := []int{2, 2}
+	fmt.Println(intersection(num1, num2))
+}
